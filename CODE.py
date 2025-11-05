d8f32e1bb6cd44eeca43cb414038041c2c3ed610
Yichi Zhang <118243283+AliciaZYC@users.noreply.github.com>
2025-11-05 12:08:03 -0800

    Update CODE.py

--- CODE.py
+++ CODE.py
@@ -1,1 +1,21 @@
+def sortBooks(books):
+    for i in range(1, len(books)):
+        curr = books[i]
+        j = i - 1
+        
+        while j >= 0 and shouldSwap(books[j], curr):
+            books[j + 1] = books[j]
+            j -= 1
+        
+        books[j + 1] = curr
+    
+    return books
 
+
+def shouldSwap(bookA, bookB):
+    if bookA['shelfNumber'] > bookB['shelfNumber']:
+        return True
+    elif bookA['shelfNumber'] == bookB['shelfNumber']:
+        if bookA['returnOrder'] > bookB['returnOrder']:
+            return True
+    return False
